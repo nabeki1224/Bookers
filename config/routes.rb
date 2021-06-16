7d85e7efa69b87b8f8fbd77fc849113e8ecf76f1
watanabe-kyo <watanabe.kyo.infratop@gmail.com>
2021-06-16 19:03:08 +0900

    登録完了メール　実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'books#top'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   get '/home/about' => 'users#about'
   #get '/users'    => 'users#users'
   # get '/books' => 'books#show'
